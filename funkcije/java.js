a62cb79baaaa2481aa16ff85d811ecc7f11fb010
sabaos <sabinmejl@gmail.com>
2023-02-22 17:25:46 +0100

    dsds

--- funkcije/java.js
+++ funkcije/java.js
@@ -7,3 +7,25 @@ ispisiIme("josipa");
 for(let i = 1; <= 5; i++){
     ispisiIme("josipa" + i);
 }
+
+//parametri
+
+function myFunc(a, b) {
+    if (!b) {
+        b = 1;
+    }
+    return a + b;
+}
+myFunc(1);
+
+
+function myFunc(a, b) {
+    b = b || 1;
+    return a+ b;
+}
+myFunc(1,1);
+
+function myFunc(a, b =1) {
+    return a + b;
+}
+myFunc(1,1)
