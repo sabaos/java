bdfae551e4ed90b5c3475095ac70074ab4683dff
sabaos <sabinmejl@gmail.com>
2023-02-22 19:27:41 +0100

    objekti

--- funkcije/java.js
+++ funkcije/java.js
@@ -1,12 +1,10 @@
-function ispisiIme(ime){
+function ispisiIme(ime) {
     console.log(ime);
 }
 ispisiIme("john doe");
 ispisiIme("josipa");
 
-for(let i = 1; <= 5; i++){
-    ispisiIme("josipa" + i);
-}
+
 
 //parametri
 
@@ -19,13 +17,64 @@ function myFunc(a, b) {
 myFunc(1);
 
 
+
 function myFunc(a, b) {
     b = b || 1;
-    return a+ b;
-}
-myFunc(1,1);
-
-function myFunc(a, b =1) {
     return a + b;
 }
-myFunc(1,1)
+myFunc(1, 1);
+
+function myFunc(a, b = 1) {
+    return a + b;
+}
+myFunc(1, 1)
+
+//funkcija kao vrijednost
+var addOne = function (value) {
+    return value + 1;
+};
+var myVar = addOne(1);
+console.log(myVar);
+
+
+//vježba
+function traziNajduzuRijec(zadaniNiz) {
+    var najduzaRijecUFunkciji = "";
+    for (let i = 0; i < zadaniNiz.length; i++) {
+        if (zadaniNiz[i].length > najduzaRijecUFunkciji.length) {
+            najduzaRijecUFunkciji = zadaniNiz[i];
+        }
+    }
+    return najduzaRijecUFunkciji;
+}
+
+function checkEmail(email) {
+    var i = email.indexOf("@");
+    console.log("search index i = " + i);
+    if (i > 0) {
+        var tmpEmailDomain = email.substring(i + 1, email.lenght);
+        console.log(tmpEmailDomain);
+        var z = email.indexOf(".");
+        console.log("Search index z =" + z);
+        if (z > 0) {
+            return true;
+        }
+        return false;
+        return true;
+    } else {
+        return false;
+    }
+}
+var isEmailOk = checkEmail("moje.ime@gmail.com");
+console.log("email OK = " + isEmailOk);
+return;
+
+
+
+var niz = ["Marko", "Ana", "Josipa", "Anamarija", "ivo"];
+var naduzeIme = traziNajduzuRijec(niz);
+
+
+
+console.log("Najduže ime je: " + naduzeIme);
+
---
+++ objekti/objekti.js
@@ -0,0 +1,30 @@
+var dog = {};
+dog.breed = 'Golden retriver';
+dog['other breed'] = 'chiuauauaa';
+
+
+
+var glavnaPasmina = dog.breed;
+var podPasmina = dog["other breed"]
+console.log("Pasmina: " + glavnaPasmina);
+console.log('Pod pasmina: ' + podPasmina);
+
+
+//*
+
+var osoba = {};
+osoba.ime = "mario"
+osoba.prezime = "sabolić";
+osoba.godine = "34";
+osoba.brojCipela = "43";
+
+/*var isObrisano = delete osoba.prezime;
+console.log(isObrisano)*/
+
+var json = JSON.stringify(osoba);
+console.log(json);
+return;
+
+if (osoba.brojCipela > 39) {
+    console.log(`ime i prezime: ${osoba.ime} ${osoba.prezime}`)
+}
